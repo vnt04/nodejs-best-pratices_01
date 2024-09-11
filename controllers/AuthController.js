f6fb06bbcc983e75d4f661680f62f4dddeac0a5c
vnt04 <84673577+vnt04@users.noreply.github.com>
2024-09-12 01:26:01 +0700

    login, jwt code

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,8 @@
 import prisma from "../DB/database.config.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import vine, { errors } from "@vinejs/vine";
-import { registerSchema } from "../validations/authValidation.js";
+import { loginSchema, registerSchema } from "../validations/authValidation.js";
 
 class AuthController {
   static async register(req, res) {
@@ -29,6 +30,48 @@ class AuthController {
       return res
         .status(201)
         .json({ message: "User created successfully", user });
+    } catch (error) {
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        return res.status(400).json({ errors: error.messages });
+      }
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const body = req.body;
+      const validator = vine.compile(loginSchema);
+      const payload = await validator.validate(body);
+
+      // find user email is login
+      const findUser = await prisma.users.findUnique({
+        where: { email: payload.email },
+      });
+      if (findUser) {
+        if (bcrypt.compareSync(payload.password, findUser.password)) {
+          const payloadData = {
+            id: findUser.id,
+            name: findUser.name,
+            email: findUser.email,
+            profile: findUser.profile,
+          };
+          const token = jwt.sign(payloadData, process.env.JWT_SECRET, {
+            expiresIn: "30s",
+          });
+          return res
+            .status(200)
+            .json({
+              message: "Successfully Login",
+              access_token: `Bearer ${token}`,
+            });
+        } else {
+          return res.json({ errors: { password: "Password is incorrect!" } });
+        }
+      }
+
+      return res
+        .status(400)
+        .json({ errors: { email: "Email can not found!" } });
     } catch (error) {
       if (error instanceof errors.E_VALIDATION_ERROR) {
         return res.status(400).json({ errors: error.messages });
--- validations/authValidation.js
+++ validations/authValidation.js
@@ -5,3 +5,8 @@ export const registerSchema = vine.object({
   email: vine.string().email(),
   password: vine.string().minLength(6).maxLength(100).confirmed(),
 });
+
+export const loginSchema = vine.object({
+  email: vine.string().email(),
+  password: vine.string(),
+});
